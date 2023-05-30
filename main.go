5dda8d76d22accc644d6b44971ff0c58c1d4f1e5
kontik501 <kontik501@gmail.com>
2023-05-30 18:10:53 +0300

    deployed

--- main.go
+++ main.go
@@ -15,15 +15,9 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
+		AllowOrigins:     "https://gofront.onrender.com, 35.160.120.126, 44.233.151.27, 34.211.200.85",
+		AllowHeaders:     "Origin, Content-Type, Accept",
 	}))
-
-	app.Use(cors.New())
-
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://gofront.onrender.com, 35.160.120.126, 44.233.151.27, 34.211.200.85",
-		AllowHeaders: "Origin, Content-Type, Accept",
-	}))
-
 	routes.Setup(app)
 
 	app.Listen(":8000")
