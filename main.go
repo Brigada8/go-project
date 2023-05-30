8698e0005d4a8a84597214408c7ff33dbea8ce1a
kontik501 <kontik501@gmail.com>
2023-05-30 17:41:40 +0300

    Adding cors

--- main.go
+++ main.go
@@ -17,6 +17,13 @@ func main() {
 		AllowCredentials: true,
 	}))
 
+	app.Use(cors.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://gofront.onrender.com, 35.160.120.126, 44.233.151.27, 34.211.200.85",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	routes.Setup(app)
 
 	app.Listen(":8000")
