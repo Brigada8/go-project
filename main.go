d7de30b57e83ff4c3f7c437ffa40e2c60a1037fb
kontik501 <kontik501@gmail.com>
2023-05-31 17:36:08 +0300

    allow origins

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowOrigins:     "*",
+		AllowOrigins:     "https://gofront.onrender.com, https://35.160.120.126, https://44.233.151.27, https://34.211.200.85",
 	}))
 	routes.Setup(app)
 
